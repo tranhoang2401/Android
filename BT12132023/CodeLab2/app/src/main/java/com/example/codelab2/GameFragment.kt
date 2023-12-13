d982626e956cee6bd394f8b394638eb42e2f31d4
hoang <anhhoang24012002@gmail.com>
2023-12-13 21:26:08 +0700

    BT12132023

---
+++ BT12132023/CodeLab2/app/src/main/java/com/example/codelab2/GameFragment.kt
@@ -0,0 +1,84 @@
+package com.example.codelab2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.codelab2.databinding.GameFragmentBinding
+class GameFragment : Fragment() {
+
+    private var score = 0
+    private var currentWordCount = 0
+    private var currentScrambledWord = "test"
+
+    private lateinit var binding: GameFragmentBinding
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout XML file and return a binding object instance
+        binding = GameFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.submit.setOnClickListener { onSubmitWord() }
+        binding.skip.setOnClickListener { onSkipWord() }
+        updateNextWordOnScreen()
+        binding.score.text = getString(R.string.score, 0)
+        binding.wordCount.text = getString(
+                R.string.word_count, 0, MAX_NO_OF_WORDS)
+    }
+
+    private fun onSubmitWord() {
+        currentScrambledWord = getNextScrambledWord()
+        currentWordCount++
+        score += SCORE_INCREASE
+        binding.wordCount.text = getString(R.string.word_count, currentWordCount, MAX_NO_OF_WORDS)
+        binding.score.text = getString(R.string.score, score)
+        setErrorTextField(false)
+        updateNextWordOnScreen()
+    }
+
+    private fun onSkipWord() {
+        currentScrambledWord = getNextScrambledWord()
+        currentWordCount++
+        binding.wordCount.text = getString(R.string.word_count, currentWordCount, MAX_NO_OF_WORDS)
+        setErrorTextField(false)
+        updateNextWordOnScreen()
+    }
+
+    /*
+     * Gets a random word for the list of words and shuffles the letters in it.
+     */
+    private fun getNextScrambledWord(): String {
+        val tempWord = allWordsList.random().toCharArray()
+        tempWord.shuffle()
+        return String(tempWord)
+    }
+
+    private fun restartGame() {
+        setErrorTextField(false)
+        updateNextWordOnScreen()
+    }
+
+    private fun exitGame() {
+        activity?.finish()
+    }
+    private fun setErrorTextField(error: Boolean) {
+        if (error) {
+            binding.textField.isErrorEnabled = true
+            binding.textField.error = getString(R.string.try_again)
+        } else {
+            binding.textField.isErrorEnabled = false
+            binding.textInputEditText.text = null
+        }
+    }
+    private fun updateNextWordOnScreen() {
+        binding.textViewUnscrambledWord.text = currentScrambledWord
+    }
+}
